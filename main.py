321658297536ee2b695f0b5333e0c939a2a9ffdd
Robbie McGregor <mr.robbie.mcgregor@gmail.com>
2022-02-09 20:09:52 +1300

    logic done for loading movie page, front page

--- main.py
+++ main.py
@@ -2,11 +2,79 @@ import os
 import requests
 from flask import Flask, render_template
 
+apiKey = '1e562fa9c998c5306da6e7b23bfd7138'
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    sections = []
+    for showcase in showcases:
+        apiResponse = apiCall(showcase['apiLink'])
+        movies = apiResponse['results']
+        for movie in movies:
+            movie['release_date'] = convertDate(movie['release_date'])
+        sections.append({
+            'title': showcase['title'],
+            'movies': movies
+        })
+    # movies = apiCall(f'https://api.themoviedb.org/3/movie/top_rated?api_key={apiKey}&language=en-US&page=1')
+    # movies = movies['results']
+
+    return render_template('index.html', showcases=sections)
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+@app.route('/movie/<id>')
+def getMovie(id):
+    movie = apiCall(f"https://api.themoviedb.org/3/movie/{id}?api_key={apiKey}&language=en-US")
+    cast = apiCall(f"https://api.themoviedb.org/3/movie/{id}/credits?api_key={apiKey}&language=en-US")
+    return render_template('movie.html', movie=movie, cast=cast)
+
+def convertDate(date):
+    if (date):
+        date = date.split('-')
+        year = date[0]
+        month = int(date[1])
+        day = date[2]
+        months = [ "January", "February", "March", "April", "May", "June", 
+            "July", "August", "September", "October", "November", "December" ]
+        monthName = months[month - 1]
+        return f"{monthName} {day}, {year}"
+    else:
+            return ""
+
+showcases = [
+    {
+        'id': 'popular',
+        'title': "What's Popular",
+        'apiLink': f"https://api.themoviedb.org/3/discover/movie?api_key={apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate"
+    },
+    {
+        'id': 'nowPlaying',
+        'title': "Now Playing",
+        'apiLink': f"https://api.themoviedb.org/3/movie/now_playing?api_key={apiKey}&language=en-US&page=1"
+    },
+    {
+        'id': 'upcoming',
+        'title': "Upcoming",
+        'apiLink': f"https://api.themoviedb.org/3/movie/upcoming?api_key={apiKey}&language=en-US&page=1"
+    },
+    {
+        'id': 'topRated',
+        'title': "Top Rated",
+        'apiLink': f"https://api.themoviedb.org/3/movie/top_rated?api_key={apiKey}&language=en-US&page=1"
+    },
+]
+
+
+def apiCall(url):
+    response = requests.get(url)
+    return response.json()
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
